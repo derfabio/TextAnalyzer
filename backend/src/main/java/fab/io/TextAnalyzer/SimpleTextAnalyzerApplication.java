8429d32d9b37859ea66dc461a0aeaf4dc24db583
derFabio <fabio.virgolini@gmail.com>
2023-10-04 19:21:18 +0200

    initial commit

---
+++ backend/src/main/java/fab/io/TextAnalyzer/SimpleTextAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package fab.io.TextAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTextAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTextAnalyzerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fab/io/TextAnalyzer/SimpleTextAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package fab.io.TextAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTextAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
